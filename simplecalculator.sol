37c60dd2aa27b3e4f87bf038772f98ff3baa859b
M A Z I <gekere615@gmail.com>
2024-02-01 19:42:49 +0100

    Update simplecalculator.sol

--- simplecalculator.sol
+++ simplecalculator.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19; //solidity version
 contract simpleCalculator {
     uint256 public result; //declared result variable
 
-    function add(uint256 a, uint256 b) external {
+    function add(uint256 a, uint256 b) external { //function to add 
         result = a + b;
     }
 
