fc942a9ac4e3d57262110ef3a90f3bd1b9397d4e
M A Z I <gekere615@gmail.com>
2024-02-01 15:21:51 +0100

    Update simplecalculator.sol

--- simplecalculator.sol
+++ simplecalculator.sol
@@ -1,19 +1,23 @@
-pragma solidity 0.8.19;
-contract calculator{
-    uint256 public c;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
-    function add(uint256 a, uint256 b) public {
-        c = a +b;
-    }
-    function subtract(uint256 a, uint256 b) public {
-        c = a - b;
-    }
-       function multiply(uint256 a, uint256 b) public {
-        c = a * b;
-    }
-       function divide(uint256 a, uint256 b) public {
-        c = a / b;
+contract Calculator {
+    uint256 public result;
+
+    function add(uint256 a, uint256 b) external {
+        result = a + b;
     }
 
+    function subtract(uint256 a, uint256 b) external {
+        result = a - b;
+    }
 
+    function multiply(uint256 a, uint256 b) external {
+        result = a * b;
+    }
+
+    function divide(uint256 a, uint256 b) external {
+        require(b != 0, "Cannot divide by zero");
+        result = a / b;
+    }
 }
