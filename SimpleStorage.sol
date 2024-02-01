892ce60d239b934bce621f97e595c2ae52278b79
M A Z I <gekere615@gmail.com>
2024-02-01 14:53:47 +0100

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,10 +1,14 @@
 pragma solidity 0.8.19;
 
 contract simplestorage{
+    //initialize favouriteNumber variable to zero
     uint256 public favouriteNumber;
 
-    function store(uint256 _favouriteNumber) public{
+    function store(uint256 _favouriteNumber) public{ //function that parses a new variable _favouriteNumber then updates the state variable  favouriteNumber by adding 1
         
-        favouriteNumber = _favouriteNumber;
+        favouriteNumber = _favouriteNumber +1;
+    }
+    function retrive() public view returns(uint256){ //here this function returns the value of the updated favouriteNumber variable without being able to modify it because of the view keyword
+        return favouriteNumber;
     }
 }
