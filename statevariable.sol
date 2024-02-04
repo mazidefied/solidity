edb470a430c8f89e96a3060f624b620f3d508802
M A Z I <gekere615@gmail.com>
2024-02-05 00:48:59 +0100

    Create statevariable.sol

---
+++ statevariable.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.8.0;
+
+contract stateVariables {
+    // State variable
+    uint public myUint = 123; // This is a state variable that is permanently stored in the contract storage. They are declared inside a contract and outside a function.
+}
