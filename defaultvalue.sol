f16d3f13412a79a79c66f7cebfe7124fe8273295
M A Z I <gekere615@gmail.com>
2024-02-05 01:53:39 +0100

    Create defaultvalue.sol

---
+++ defaultvalue.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.0;
+contract defaultValues{
+    bool public b;// false
+    uint256 public u;//0
+    int public i;//0
+    address public a;//0x0000000000000000000000000000000000000000
+    bytes32 public b32; //0x0000000000000000000000000000000000000000000000000000000000000000
+}
