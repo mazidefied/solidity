53ea566104f27fd8cbb86adc314890b52a5313b4
M A Z I <gekere615@gmail.com>
2024-02-07 08:21:21 +0100

    Update ifelse.sol

--- ifelse.sol
+++ ifelse.sol
@@ -1,11 +1,26 @@
 pragma solidity ^0.8.0;
-contract ifelse{
-    function example(uint256 x) external pure returns(uint256){
-        if(x<10){
-            return 0;
-        }else{
-            return 1;
 
+contract ifelse {
+    function example(uint256 x) external pure returns (uint256) {
+        if (x < 10) {
+            return 0;
+        } else if (x < 20) {
+            return 1;
+        } else {
+            return 2;
         }
+    }
+    function ternary(uint256 x) external pure returns (uint256){
+ //       if(x<10){
+   //         return 0;
+     //   } 
+       //     return 1;
+
+       //now i tried to implement the ifelse in a single line of code
+    return x < 10?1:2; // if x is less than 10, the function will return 1, else it will return 2
+    }
+//just another implementation to be sure
+    function excersize(uint256 x) external pure returns(uint256){
+        return x< 10?1:2;
     }
 }
